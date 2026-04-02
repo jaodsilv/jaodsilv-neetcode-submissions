dd5ad983c80d3a3823dd46201031fabc8d934e9b
joão marco maciel da silva <jaodsilv@users.noreply.github.com>
2026-04-02 00:40:34 -0300

    Add: accounts-merge - submission-1

---
+++ Data Structures & Algorithms/accounts-merge/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        accounts.sort(key=lambda x: x[0]) # O(nlogn)
+        accs = [[x[0], set(x[1:])] for x in accounts] #O(n*m)
+        #Now we have all accounts with same names together
+        res = []
+        i = 0
+        prevName = None
+        hasMerge = True
+        while hasMerge:
+            hasMerge = False
+            for acc in accs:
+                name = acc[0]
+                if name != prevName:
+                    prevName = name
+                    i = len(res)
+                    res.append(acc)
+                else:
+                    emails = acc[1]
+                    mergeCurrent = False
+                    for j in range(i, len(res)):
+                        otherEmails = res[j][1]
+                        if otherEmails & emails:
+                            res[j][1] |= emails
+                            hasMerge = True
+                            mergeCurrent = True
+                            break
+                    if not mergeCurrent:
+                        res.append(acc)
+            accs = res
+            res = []
+        return [x[:1] + sorted(x[1]) for x in accs]
+
