82ec8274c9a314b3633c4e449fa7ecbdb5cc35c7
joão marco maciel da silva <jaodsilv@users.noreply.github.com>
2026-04-12 00:09:26 -0300

    Add: minimum-window-with-characters - submission-16

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-16.py
@@ -0,0 +1,44 @@
+from collections import defaultdict
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        counter = defaultdict(int) # O(26)
+        for c in t:
+            counter[c] += 1
+            
+        gt0 = len(counter)
+        minLen = len(s) + 1
+        minLenL = -1
+        minLenR = len(s) + 1
+        R = 0
+        while R < len(s) and s[R] not in counter:
+            R += 1
+        L = R
+
+        # print(0, L, R, minLenL, minLenR, gt0, s[minLenL:minLenR])
+        while R < len(s):
+            while R < len(s) and gt0 > 0:
+                if s[R] in counter:
+                    counter[s[R]] -= 1
+                    if counter[s[R]] == 0:
+                        gt0 -= 1
+                R += 1
+            # print(1, L, R, minLenL, minLenR, gt0, s[minLenL:minLenR])
+            while gt0 == 0:
+                if s[L] in counter:
+                    counter[s[L]] += 1
+                    if counter[s[L]] == 1:
+                        gt0 += 1
+                        if R-L < minLen:
+                            minLen = R-L
+                            minLenL = L
+                            minLenR = R
+                L += 1
+            # print(2, L, R, minLenL, minLenR, gt0, s[minLenL:minLenR])
+
+        if gt0 == 0 and R-L < minLen:
+            minLenL, minLenR = L, R
+            # print(3, L, R, minLenL, minLenR, gt0, s[minLenL:minLenR])
+        if minLenR == len(s) + 1:
+            return ""
+        else:
+            return s[minLenL:minLenR]
