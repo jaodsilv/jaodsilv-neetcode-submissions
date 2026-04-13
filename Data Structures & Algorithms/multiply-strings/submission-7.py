669be4b8d8ead4b64e142cc045c0641ca3ffa652
joão marco maciel da silva <jaodsilv@users.noreply.github.com>
2026-04-13 12:57:48 -0300

    Add: multiply-strings - submission-7

---
+++ Data Structures & Algorithms/multiply-strings/submission-7.py
@@ -0,0 +1,53 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num1[0] == '0' or num2[0] == '0':
+            return '0'
+        if len(num1) < len(num2):
+            num1, num2 = num2, num1
+        
+        dp = [[] for _ in range(10)]
+        dp[0] = [0]
+        def inty(d: str):
+            return ord(d) - ord('0')
+        num1List = [inty(d) for d in num1][::-1]
+        num2List = [inty(d) for d in num2][::-1]
+
+        def multiply_digit(digit: int, trail0s: int):
+            if digit == 0:
+                return [0]
+            if dp[digit]:
+                return [0]*trail0s + dp[digit]
+            res = []
+            up = 0
+            for d in num1List:
+                up = digit*d + up
+                res.append(up % 10)
+                up //= 10
+            if up:
+                res.append(up)
+            dp[digit] = res
+            return [0]*trail0s + res
+        def sum(A, B):
+            if B == '0':
+                return
+            if len(A) < len(B):
+                A.extend([0]*(len(B) - len(A)))
+            up = 0
+            for i in range(len(A)):
+                a, b = A[i], B[i]
+                up = a + b + up
+                A[i] = up % 10
+                up //= 10
+            if up:
+                A.append(up)
+        res = []
+        trail0s = 0
+        for i in num2List:
+            B = multiply_digit(i, trail0s)
+            print('B', B[::-1])
+            sum(res, B)
+            print('res', res[::-1])
+            trail0s += 1
+        while res[-1] == 0:
+            res.pop()
+        return ''.join([str(d) for d in res][::-1])
