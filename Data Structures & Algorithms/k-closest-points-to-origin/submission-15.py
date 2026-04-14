880b01e814ed478f5fc2438b055d6a805004466f
joão marco maciel da silva <jaodsilv@users.noreply.github.com>
2026-04-14 18:44:15 -0300

    Add: k-closest-points-to-origin - submission-15

---
+++ Data Structures & Algorithms/k-closest-points-to-origin/submission-15.py
@@ -0,0 +1,62 @@
+import math
+import random
+
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        if k >= len(points):
+            return points
+        def dist_center(x, y) -> float:
+            return math.sqrt(x*x + y*y)
+    
+        def pivot(dists, L, R) -> tuple[int, float]:
+            return random.choice(dists[L:R])
+            # return (dists[0] + dists[-1]) // 2
+            # d0 = dists[0]
+            # d1 = dists[len(dists) // 2]
+            # d2 = dists[-1]
+            # return (d0+d1+d2) / 3
+
+        # First let's compute once all the distances
+        dists = []
+        for i in range(len(points)):
+            dists.append(dist_center(points[i][0], points[i][1]))
+        # Now let's quick-search style divide the array into the k closest vs the n-k farthest
+
+        L, R = 0, len(points)
+        while L != k and R != k:
+            # if L >= R:
+            #     return 0
+            p = pivot(dists, L, R)
+            i = L
+            prevL, prevR = L, R
+            while i < R:
+                if dists[i] < p:
+                    dists[i], dists[L] = dists[L], dists[i]
+                    points[i], points[L] = points[L], points[i]
+                    L += 1
+                    i += 1
+                elif dists[i] > p:
+                    R -= 1
+                    dists[i], dists[R] = dists[R], dists[i]
+                    points[i], points[R] = points[R], points[i]
+                else:
+                    i += 1
+            # Dists[:L] are all less than p
+            # Dists[L:R] have all the same distance = p
+            # Dists[R:] are all greater than p
+            if R < k:
+                L = R
+                R = prevR
+            elif R == k:
+                return points[:R]
+            elif L < k: # R > k
+                # process from L to R
+                pass
+            elif L == k:
+                return points[:L]
+            else: # L > k
+                R = L
+                L = prevL
+            print(L, R, k, prevL, prevR, p)
+
+        return points[:L]
