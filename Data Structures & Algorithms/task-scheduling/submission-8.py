ac97d02e0288517052a42bf70167ca122ab91beb
joão marco maciel da silva <jaodsilv@users.noreply.github.com>
2026-04-18 18:05:15 -0300

    Add: task-scheduling - submission-8

---
+++ Data Structures & Algorithms/task-scheduling/submission-8.py
@@ -0,0 +1,14 @@
+from collections import Counter
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        most_common = Counter(tasks).most_common()
+        _, top_counter = most_common[0]
+        distinct = len(most_common)
+        top_extras = 0
+        while top_extras < distinct and most_common[top_extras][1] == top_counter:
+            top_extras += 1
+        print(top_extras)
+        if len(tasks) > (top_counter-1) * (n + 1) + 1:
+            return len(tasks)
+        else:
+            return (top_counter-1) * (n+1) + top_extras
