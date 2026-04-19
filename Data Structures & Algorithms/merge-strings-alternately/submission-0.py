a3b43c0622521e5edc6ad350d9f762079c0f9722
joão marco maciel da silva <jaodsilv@users.noreply.github.com>
2026-04-19 13:56:52 -0300

    Add: merge-strings-alternately - submission-0

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        res = []
+        for a, b in zip(word1, word2):
+            res.append(a)
+            res.append(b)
+        cut = min(len(word1), len(word2))
+        res.append(word1[cut:])
+        res.append(word2[cut:])
+        return ''.join(res)
