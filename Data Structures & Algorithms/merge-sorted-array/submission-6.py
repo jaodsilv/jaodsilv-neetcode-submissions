d2f69db428498e62a9c920f00e48a6325945b7e3
joão marco maciel da silva <jaodsilv@users.noreply.github.com>
2026-04-20 18:18:54 -0300

    Add: merge-sorted-array - submission-6

---
+++ Data Structures & Algorithms/merge-sorted-array/submission-6.py
@@ -0,0 +1,19 @@
+from collections import deque
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if n == 0:
+            return
+        last = m+n-1
+        m -= 1
+        n -= 1
+        while n >= 0:
+            if m >= 0 and nums1[m] > nums2[n]:
+                nums1[last] = nums1[m]
+                m -= 1
+            else:
+                nums1[last] = nums2[n]
+                n -= 1
+            last -= 1
