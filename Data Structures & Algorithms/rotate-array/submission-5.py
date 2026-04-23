3fb01da6d87f3f43cb142963f19a465d1aaa3d75
joão marco maciel da silva <jaodsilv@users.noreply.github.com>
2026-04-23 13:59:14 -0300

    Add: rotate-array - submission-5

---
+++ Data Structures & Algorithms/rotate-array/submission-5.py
@@ -0,0 +1,22 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        '''
+        input: nums=[100,200,300,400,500,600], k=4
+        nums.reverse() => nums=[600,500,400,300,200,100]
+        nums[:k].reverse() => nums=[300,400,500,600,200,100]
+        nums[k:].reverse() => nums=[300,400,500,600,100,200]
+        pos[i] when reversed goes to n-i-1
+        if n-i-1 is < k:
+            it then goes to k+i-n
+        else:
+            it then goes to k + (n-k)-(n-i-1-k)-1 = k+i
+        '''
+        n = len(nums)
+        k = k % n
+
+        nums.reverse()
+        nums[:k] = nums[:k][::-1]
+        nums[k:] = nums[k:][::-1]
