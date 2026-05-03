7deb286ab83673f9da9ad7d62ad131596c69e4b2
joão marco maciel da silva <jaodsilv@users.noreply.github.com>
2026-05-03 14:40:01 -0300

    Add: cheapest-flight-path - submission-7

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-7.py
@@ -0,0 +1,22 @@
+from collections import defaultdict
+import heapq
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        # Let's build the graph first
+        graph = [[] for _ in range(n+1)]
+        heap = []
+        for srcc, dstt, price in flights:
+            graph[srcc].append((price, dstt))
+            if srcc == src:
+                heapq.heappush(heap, (price, 0, dstt))
+
+        # Now we do a modified dijkstra counting the stops
+        # Note that we may add a flight more than once in the heap, but with different number of steps
+        while heap:
+            price, stops, stop = heapq.heappop(heap)
+            if stop == dst:
+                return price
+            if stops < k:
+                for legprice, dstt in graph[stop]:
+                    heapq.heappush(heap, (price+legprice, stops+1, dstt))
+        return -1
